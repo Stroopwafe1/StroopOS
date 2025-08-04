5b2ac9597f6b3a1dd26645d9842c3b128d379368
Stroopwafe1 <48443491+Stroopwafe1@users.noreply.github.com>
2025-08-04 13:22:47 +0200

    Feat: Migrate to Multiboot2
    Signed-off-by: Stroopwafe1 <48443491+Stroopwafe1@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -6,6 +6,10 @@ StroopOS.iso: grub.cfg StroopOS.bin
 	cp grub.cfg build/iso/boot/grub/grub.cfg
 	grub-mkrescue -d $(GRUB2_CROSS_PC) -o StroopOS.iso build/iso
 
+debug: StroopOS.iso StroopOS.bin
+	qemu-system-i386 -cdrom StroopOS.iso -s -S &
+	gdb -ex 'target remote localhost:1234' StroopOS.bin
+
 test: StroopOS.bin
 	qemu-system-i386 -kernel StroopOS.bin
 
@@ -13,10 +17,10 @@ test_iso: StroopOS.iso
 	qemu-system-i386 -cdrom StroopOS.iso
 
 StroopOS.bin: kernel.o boot.o linker.ld
-	i686-elf-gcc -T linker.ld -o StroopOS.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+	i686-elf-gcc -T linker.ld -o StroopOS.bin -ffreestanding -ggdb -nostdlib boot.o kernel.o -lgcc
 
 kernel.o: kernel.c
-	i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+	i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -ggdb -Wall -Wextra
 
 boot.o: boot.asm
-	yasm -felf32 boot.asm -o boot.o
+	yasm -DCONSOLE_MODE -gdwarf2 -felf32 boot.asm -o boot.o
--- boot.asm
+++ boot.asm
@@ -1,9 +1,22 @@
 MBALIGN equ 1 << 0			; align loaded (Multiboot) modules on page boundaries
 MEMINFO equ 1 << 1			; provide memory mapping
 MBFLAGS equ MBALIGN | MEMINFO
-MAGIC equ 0x1BADB002		; multiboot header
-CHECKSUM equ -(MAGIC + MBFLAGS)
+MBARCH equ 0					; 0 means 32-bit (protected) mode i386
+MBHEADER_LENGTH equ mb_header_end - mb_header_begin	
+MAGIC equ 0xE85250D6		; multiboot header
+CHECKSUM equ -(MAGIC + MBARCH + MBHEADER_LENGTH)
 
+
+MB_TAG_FLAGS_TYPE equ 4
+MB_TAG_FLAGS_SIZE equ 12
+
+MB_TAG_FB_TYPE equ 5
+MB_TAG_FB_SIZE equ 20
+
+MB_TAG_TERM_TYPE equ 0
+MB_TAG_TERM_SIZE equ 8
+	
+MB_TAG_OPTIONAL equ 0
 ; Declare a multiboot header that marks the program as a kernel. These are magic
 ; values that are documented in the multiboot standard. The bootloader will
 ; search for this signature in the first 8 KiB of the kernel file, aligned at a
@@ -11,9 +24,33 @@ CHECKSUM equ -(MAGIC + MBFLAGS)
 ; forced to be within the first 8 KiB of the kernel file.
 section .multiboot
 align 4
+mb_header_begin:
 	dd MAGIC
-	dd MBFLAGS
+	dd MBARCH
+	dd MBHEADER_LENGTH
 	dd CHECKSUM
+
+align 8
+	%ifdef CONSOLE_MODE
+	dw MB_TAG_FLAGS_TYPE
+	dw MB_TAG_OPTIONAL
+	dd MB_TAG_FLAGS_SIZE
+	dd MBFLAGS
+	%endif
+
+align 8
+	dw MB_TAG_FB_TYPE
+	dw MB_TAG_OPTIONAL
+	dd MB_TAG_FB_SIZE
+	dd 1024
+	dd 768
+	dd 32
+
+align 8
+	dw MB_TAG_TERM_TYPE
+	dw MB_TAG_OPTIONAL
+	dw MB_TAG_TERM_SIZE
+mb_header_end:	
 
 section .bss
 	align 16
@@ -22,7 +59,7 @@ stack_bottom:
 stack_top:	
 
 section .text
-global _start;:function (_start.end - _start)
+global _start:function (_start.end - _start)
 
 _start:
 	mov esp, stack_top
