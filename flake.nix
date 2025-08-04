68297eb2d6bfc53a4c0f27bea378243c98ce6a61
Stroopwafe1 <48443491+Stroopwafe1@users.noreply.github.com>
2025-08-04 19:57:45 +0200

    Chore: More reproducible

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+      {
+        devShells.x86_64-linux.default = pkgs.mkShell {
+          GRUB2_CROSS_PC="${pkgs.pkgsCross.i686-embedded.buildPackages.grub2}/lib/grub/i386-pc/";
+          nativeBuildInputs =
+            with pkgs; [
+              pkgsCross.i686-embedded.buildPackages.gcc
+              pkgsCross.i686-embedded.buildPackages.grub2
+              libisoburn
+            ];        
+        };
+      };
+}
